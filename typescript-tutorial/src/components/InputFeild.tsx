28b091f2968750873ac70a6113db971eb373293c
KazunoriHiguchi <kazunorihiguchi@keio.jp>
2023-04-17 16:10:34 +0900

    passing props to components

--- typescript-tutorial/src/components/InputFeild.tsx
+++ typescript-tutorial/src/components/InputFeild.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
 import "./style.css";
 
-const InputFeild = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const InputFeild: React.FC<Props> = ({ todo, setTodo }) => {
+
   return (
     <div className="input">
-      <input type="input" placeholder="Enter a task" className="input__box"></input>
+      <input type="input" placeholder="Enter a task" className="input__box" value={todo} onChange={(e) => setTodo(e.target.value)}></input>
       <button className="input_submit" type="submit">Go</button>
     </div>
   )
