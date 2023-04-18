e94c46fd61f6e257f26fe662dd82363003af9b08
KazunoriHiguchi <kazunorihiguchi@keio.jp>
2023-04-18 12:57:48 +0900

    passing function as prop

--- typescript-tutorial/src/App.tsx
+++ typescript-tutorial/src/App.tsx
@@ -1,16 +1,20 @@
 import React, {useState} from 'react';
 import './App.css';
 import InputFeild from './components/InputFeild';
+import { Todo } from "./model";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = () => {};
 
   console.log(todo);
 
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputFeild todo={todo} setTodo={setTodo}/>
+      <InputFeild todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
     </div>
   );
 }
--- typescript-tutorial/src/components/InputFeild.tsx
+++ typescript-tutorial/src/components/InputFeild.tsx
@@ -4,15 +4,16 @@ import "./style.css";
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: () => void;
 }
 
-const InputFeild: React.FC<Props> = ({ todo, setTodo }) => {
+const InputFeild: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
 
   return (
-    <div className="input">
+    <form className="input" onSubmit={handleAdd}>
       <input type="input" placeholder="Enter a task" className="input__box" value={todo} onChange={(e) => setTodo(e.target.value)}></input>
       <button className="input_submit" type="submit">Go</button>
-    </div>
+    </form>
   )
 }
 
